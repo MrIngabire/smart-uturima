1ed1f9dd50972c464586994a169003d574321b63
MrIngabire <162576803+MrIngabire@users.noreply.github.com>
2026-05-08 11:14:01 +0000

    fixed database tables and added initial data

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "uturima_api",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -115,3 +117,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+# Add these lines to fix the 403 Forbidden error in Codespaces
+ALLOWED_HOSTS = ['*']
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.app.github.dev',
+    'https://*.preview.app.github.dev',
+    'http://127.0.0.1',
+    'http://localhost',
+]
--- backend/uturima_api/admin.py
+++ backend/uturima_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Garden, CropRecord, MaintenanceTask
 
-# Register your models here.
+# This makes your tables appear in the admin dashboard
+admin.site.register(Garden)
+admin.site.register(CropRecord)
+admin.site.register(MaintenanceTask)
